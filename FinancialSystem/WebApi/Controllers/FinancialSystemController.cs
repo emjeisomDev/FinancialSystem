f8b9f450fa4801f1bde50e2adedde075f4defb8e
emjeisomDev <emjeisom.dev@gmail.com>
2023-08-26 19:28:23 -0300

    Creation of System management APIs

---
+++ FinancialSystem/WebApi/Controllers/FinancialSystemController.cs
@@ -0,0 +1,72 @@
+﻿using Domain.Interfaces.IFinancialSystem;
+using Domain.Interfaces.InterfaceServices;
+using Entities.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class FinancialSystemController : ControllerBase
+    {
+        private readonly FinancialSystemInterface _FinancialSystemInterface;
+        private readonly IFinancialSystemService _IFinancialSystemService;
+
+        public FinancialSystemController(
+            FinancialSystemInterface FinancialSystemInterface, 
+            IFinancialSystemService IFinancialSystemService)
+        {
+            _FinancialSystemInterface = FinancialSystemInterface;
+            _IFinancialSystemService = IFinancialSystemService;
+        }
+
+        [HttpGet("/api/ListUserSystem")]
+        [Produces("application/json")]
+        public async Task<object> ListUserSystem(string userEmail)
+            => await _FinancialSystemInterface.ListUserSystem(userEmail);
+
+        [HttpGet("/api/GetEntityById")]
+        [Produces("application/json")]
+        public async Task<object> GetEntityById(int id)
+            => await _FinancialSystemInterface.GetEntityById(id);
+
+        [HttpPost("/api/AddFiancialSystem")]
+        [Produces("application/json")]
+        public async Task<object> AddFiancialSystem(FinancialSystem financialSystem)
+        {
+            await _IFinancialSystemService.AddFiancialSystem(financialSystem);
+            return Task.FromResult(financialSystem);
+        }
+
+        [HttpPut("/api/UpdateFiancialSystem")]
+        [Produces("application/json")]
+        public async Task<object> UpdateFiancialSystem(FinancialSystem financialSystem)
+        {
+            await _IFinancialSystemService.UpdateFiancialSystem(financialSystem);
+            return Task.FromResult(financialSystem);
+        }
+
+        [HttpDelete("/api/DeleteFiancialSystem")]
+        [Produces("application/json")]
+        public async Task<object> DeleteFiancialSystem(int id)
+        {
+            try
+            {
+                var financialSystem = await _FinancialSystemInterface.GetEntityById(id);
+                await _FinancialSystemInterface.Delete(financialSystem);
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+            return true;
+        }
+
+
+
+
+
+    }
+}
---
+++ FinancialSystem/WebApi/Controllers/UserFinancialSystemController.cs
@@ -0,0 +1,71 @@
+﻿using Domain.Interfaces.InterfaceServices;
+using Domain.Interfaces.IUserFinancialSystem;
+using Entities.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class UserFinancialSystemController : ControllerBase
+    {
+        private readonly UserFinancialSystemInterface _UserFinancialSystemInterface;
+        private readonly IUserFinancialSystemService _IUserFinancialSystemService;
+
+        public UserFinancialSystemController(
+            UserFinancialSystemInterface UserFinancialSystemInterface, 
+            IUserFinancialSystemService IUserFinancialSystemService)
+        {
+            _UserFinancialSystemInterface = UserFinancialSystemInterface;
+            _IUserFinancialSystemService = IUserFinancialSystemService;
+        }
+
+        [HttpGet("/api/ListUsersSystem")]
+        [Produces("application/json")]
+        public async Task<object> ListUsersSystem(int idSystem) 
+            => await _UserFinancialSystemInterface.ListUsersSystem(idSystem);
+
+        [HttpPost("/api/AddUserFinancialSystem")]
+        [Produces("application/json")]
+        public async Task<object> AddUserFinancialSystem(int idSystem, string userEmail)
+        {
+            try
+            {
+                await _IUserFinancialSystemService.AddUserFinancialSystem(
+                        new UserFinancialSystem
+                        {
+                            IdSystem = idSystem,
+                            UserEmail = userEmail,
+                            Admin = false,
+                            CurrentSystem = true
+                        });
+            }
+            catch (Exception)
+            {
+                return Task.FromResult(false);
+            }
+            return Task.FromResult(true);
+        }
+
+        [HttpDelete("/api/DeleteUserFinancialSystem")]
+        [Produces("application/json")]
+        public async Task<object> DeleteUserFinancialSystem(int idSystem)
+        {
+            try
+            {
+                var userFinancialSystem = await _UserFinancialSystemInterface.GetEntityById(idSystem);
+                await _UserFinancialSystemInterface.Delete(userFinancialSystem);
+            }
+            catch (Exception)
+            {
+                return Task.FromResult(false);
+            }
+            return Task.FromResult(true);
+        }
+
+
+
+    }
+}
