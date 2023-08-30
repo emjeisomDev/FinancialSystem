145b8ced5ea8c3c5b8ffb8c8ae1ee7ab19257fb9
emjeisomDev <emjeisom.dev@gmail.com>
2023-08-30 10:07:14 -0300

    Create angular project

--- FinancialSystem/WebApi/Program.cs
+++ FinancialSystem/WebApi/Program.cs
@@ -81,6 +81,13 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+var devClient = "http://localhost:4200";
+app.UseCors(x =>
+    x.AllowAnyOrigin()
+    .AllowAnyMethod()
+    .AllowAnyHeader()
+    .WithOrigins(devClient));
+
 app.UseHttpsRedirection();
 app.UseAuthentication();
 app.UseAuthorization();
