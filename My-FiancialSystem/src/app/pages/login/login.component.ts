4c707e46b991f185af316acc14ed482a5f9966dc
emjeisomDev <emjeisom.dev@gmail.com>
2023-09-04 09:49:46 -0300

    JWT Authentication Api Call

--- My-FiancialSystem/src/app/pages/login/login.component.ts
+++ My-FiancialSystem/src/app/pages/login/login.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { LoginService } from 'src/app/services/login.service';
+
 
 @Component({
   selector: 'app-login',
@@ -9,25 +12,45 @@ import { Router } from '@angular/router';
 })
 export class LoginComponent {
 
-    constructor(public formBuilder: FormBuilder, private router: Router){}
+  constructor(public formBuilder: FormBuilder,
+    private router: Router,
+    private loginService: LoginService) {
 
-    loginForm!: FormGroup;
+  }
 
-    ngOnInit():void{
-        this.loginForm = this.formBuilder.group(
-            {
-                email: ['',[Validators.required, Validators.email] ],
-                password: ['', [Validators.required]]
-            }
-        )
-    }
+  loginForm!: FormGroup;
 
-    get formData(){
-        return this.loginForm.controls;
-    }
+  ngOnInit(): void {
 
-    loginUser(){
-        alert("Ok");
-    }
+    this.loginForm = this.formBuilder.group
+      (
+        {
+          email: ['', [Validators.required, Validators.email]],
+          password: ['', [Validators.required]]
+        }
+      )
+  }
+
+
+  get dadosForm() {
+    return this, this.loginForm.controls;
+  }
+
+
+  loginUser() {
+
+    this.loginService.login(this.dadosForm["email"].value, this.dadosForm["password"].value).subscribe(
+      token => {
+        alert(token);
+        this.router.navigate(['/dashboard']);
+      },
+      err => {
+        alert('Ocorreu um erro');
+      }
+
+    )
+
+  }
+
 
 }
